a77587bda5663106747805b48da06ad9e18b1904
Brian Park <bcpark@ncsu.edu>
2023-03-31 21:39:09 -0400

    update goruped conv naive implementation in pytorcH

--- grouped_conv.py
+++ grouped_conv.py
@@ -26,36 +26,73 @@ def torch_conv2d(input, output, weight, bias, kernel, stride, padding, groups, x
     return y
 
 
-def naive_conv2d(input, weight, bias, kernel, stride, padding, groups, x, W, B):
+def naive_conv2d(input, weight, bias, kernel, stride, padding, groups, x, Weight, Bias):
     # naive convolution algorithm in pytorch
     # Weights is W, Bias is B, input is x
     # y is the output to be returned
-    
-    # compute the output shape
-    output_shape = (x.shape[0], W.shape[0], x.shape[2]//stride[0], x.shape[3]//stride[1])
-    # initialize the output
-    output = torch.zeros(output_shape)
 
-    ## Pad X if needed
-    if padding[0] != 0:
-        X_pad = torch.zeros((x.shape[0], x.shape[1], x.shape[2]+2*padding[0], x.shape[3]+2*padding[1]))
-    else:
-        X_pad = x
-        
-    ## Convolve
-    for i in range(output.shape[0]):
-        for j in range(output.shape[1]):
-            for k in range(output.shape[2]):
-                for l in range(output.shape[3]):
-                    # get the current slice
-                    slice = X_pad[i, :, k*stride[0]:k*stride[0]+kernel[0], l*stride[1]:l*stride[1]+kernel[1]]
-                    # get the current weight
-                    weight = W[j, :, :, :]
-                    # compute the dot product
-                    output[i, j, k, l] = torch.sum(slice * weight)
-  
-  
+    # input: input tensor with shape (N, C, H, W)
+    # filters: filter tensor with shape (O, I, K_h, K_w)
+
+    stride_h = stride[0]
+    stride_w = stride[1]
+    pad_h = padding[0]
+    pad_w = padding[1]
+
+    num_groups = groups
+    N, C, H, W = input
+    O, I_group, K_h, K_w = weight
+    I = I_group * num_groups
+
+    N, C, H, W = x.shape
+    O, I_group, K_h, K_w = Weight.shape
+    assert (
+        C % num_groups == 0
+    ), f"Input channels ({C}) must be divisible by number of groups ({num_groups})"
+    assert (
+        I_group * num_groups == C
+    ), f"Input channels ({C}) must equal num_groups ({num_groups}) times I_group ({I_group})"
+    assert (
+        O % num_groups == 0
+    ), f"Output channels ({O}) must be divisible by number of groups ({num_groups})"
+
+    # Compute output dimensions
+    out_h = (H - K_h + 2 * pad_h) // stride_h + 1
+    out_w = (W - K_w + 2 * pad_w) // stride_w + 1
+
+    # Allocate output array
+    output = torch.zeros((N, O, out_h, out_w)) + Bias.view(1, O, 1, 1)
+
+    # Pad input
+    padded_input = torch.nn.functional.pad(
+        x, (pad_w, pad_w, pad_h, pad_h), mode="constant"
+    )
+
+    # Perform grouped convolution
+    for n in range(N):
+        for g in range(num_groups):
+            # Divide input and filters into groups
+            i_group = padded_input[n, g * I_group : (g + 1) * I_group, :, :]
+            k_group = Weight[g * O // num_groups : (g + 1) * O // num_groups, :, :, :]
+
+            # Perform convolution on group
+            for o in range(g * O // num_groups, (g + 1) * O // num_groups):
+                for h in range(out_h):
+                    for w in range(out_w):
+                        for i in range(I_group):
+                            # Compute dot product of kernel and image patch
+                            patch_h_start = h * stride_h
+                            patch_w_start = w * stride_w
+                            patch = i_group[
+                                i,
+                                patch_h_start : patch_h_start + K_h,
+                                patch_w_start : patch_w_start + K_w,
+                            ]
+                            output[n, o, h, w] += torch.sum(
+                                patch * k_group[o - g * O // num_groups, i, :, :]
+                            )
     return output
+
 
 def check_naive_conv2d(input, output, weight, bias, kernel, stride, padding, groups):
     ### Implement naive convolution
@@ -69,22 +106,29 @@ def check_naive_conv2d(input, output, weight, bias, kernel, stride, padding, gro
         dtype=torch.float32,
     )
     x = torch.randn(input)
+    with torch.no_grad():
+        y_conv = conv(x)
 
-    y = naive_conv2d(
-        input,
-        weight,
-        bias,
-        kernel,
-        stride,
-        padding,
-        groups,
-        x,
-        conv.weight,
-        conv.bias,
-    )
+    with torch.no_grad():
+        y = naive_conv2d(
+            input,
+            weight,
+            bias,
+            kernel,
+            stride,
+            padding,
+            groups,
+            x,
+            conv.weight,
+            conv.bias,
+        )
 
     ### Compare with PyTorch
-    assert y.shape == output
+    print("Expected: ", input, output)
+    print("PyTorch : ", x.shape, y_conv.shape)
+    print("Mine    : ", x.shape, y.shape)
+
+    assert torch.allclose(y_conv, y, atol=1e-5)
 
 
 if __name__ == "__main__":
